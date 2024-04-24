6b8ea827eb492e7569b38450d91cbf33b657fdd5
Денис Колесников <73371887@mail.ru>
2024-04-24 20:34:54 +0300

    hw8

---
+++ src/main/java/pro/sky/bookemployee/BookEmployeeApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.bookemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookEmployeeApplication.class, args);
+    }
+
+}
